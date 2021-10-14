e5d53a91c6649fccc168e60f2487be996b68ee65
Patrycja <mysiak.patrycja@gmail.com>
2021-10-14 18:02:37 +0200

    upgraded calculator created

--- calculator.js
+++ calculator.js
@@ -1,25 +1,66 @@
-function add(a, b) {
-    return (a + b);
+"use strict"
+
+function add(addArray) {
+    let result = 0
+
+    for (let i = 0; i < addArray.length; i++) {
+        result = result + addArray[i];
+    }
+
+    return result
 };
 
-function subtract(a, b) {
-    return (a - b);
+function subtract(subArray) {
+    let result;
+
+    for (let i = 0; i < subArray.length; i = i + 1) {
+        if (i === 0) {
+            result = subArray[0];
+        } else {
+        result = result - subArray[i];
+        }
+
+    }
+
+    return result;
 };
 
-function multiply(a, b) {
-    return (a * b);
+function multiply(multiplyArray) {
+    let result;
+
+    for (let i = 0; i < multiplyArray.length; i++) {
+        if (i === 0) {
+            result = multiplyArray[0];
+         } else {
+            result = result * multiplyArray[i];
+        }
+
+    }
+
+    return result;
 };
 
-function divide(a, b) {
-    return (a / b);
+function divide(divideArray) {
+    let result;
+
+    for (let i = 0; i < divideArray.length; i++) {
+        if (i === 0) {
+            result = divideArray[0]; 
+        } else {
+            result = result / divideArray[i];
+        }
+
+    } 
+    return result;
 };
 
-console.log(add(6, 7));
+console.log(add([6, 7, 8, 3]));
 
-console.log(subtract(5, 2));
+console.log(subtract([25, 2, 6, 6]));
 
-console.log(multiply(10, 6));
+console.log(multiply([1, 6, 5 ,2]));
 
-console.log(divide(9, 3));
+console.log(divide([120, 2, 3, 5]));
+
 
 
