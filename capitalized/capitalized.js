c9844c6982a6445d226d4b477e3e863ae5c22b11
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-01-08 01:50:37 +0100

    created function that returns strings with capitalized words. Didn't find a way to connectc them yet

--- capitalized/capitalized.js
+++ capitalized/capitalized.js
@@ -1,7 +1,28 @@
 "use strict"
 
-function titleCase() {
+function titleCase(str) {
+
+    str = "I'm a little tea pot"
+
+    let fullLowerCase = str.toLowerCase()
+    console.log(fullLowerCase)
+
+    let splitedSentence = fullLowerCase.split(" ")
+    //console.log(splitedSentence)
+
+    let firstLetterUpperCase = 0
+    let allUpperCase 
+
+    for(let i = 0; i < splitedSentence.length; i++) {
+        if(splitedSentence[i]) {
+            firstLetterUpperCase = splitedSentence[i].charAt(0).toUpperCase() + splitedSentence[i].slice(1)
+
+        }
+           console.log(firstLetterUpperCase)
+    }
 
 }
 
-module.exports = {titleCase}
+titleCase()
+
+// module.exports = {titleCase}
