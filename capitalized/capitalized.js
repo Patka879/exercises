a605c9606c91dc296ec379796670eb583e8aae94
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-01-08 19:08:00 +0100

    created function returning sentence with every word starting in upper case

--- capitalized/capitalized.js
+++ capitalized/capitalized.js
@@ -1,28 +1,18 @@
 "use strict"
 
 function titleCase(str) {
-
-    str = "I'm a little tea pot"
-
     let fullLowerCase = str.toLowerCase()
-    console.log(fullLowerCase)
-
     let splitedSentence = fullLowerCase.split(" ")
-    //console.log(splitedSentence)
-
-    let firstLetterUpperCase = 0
-    let allUpperCase 
-
+    
     for(let i = 0; i < splitedSentence.length; i++) {
         if(splitedSentence[i]) {
-            firstLetterUpperCase = splitedSentence[i].charAt(0).toUpperCase() + splitedSentence[i].slice(1)
+            splitedSentence[i] = splitedSentence[i].charAt(0).toUpperCase() + splitedSentence[i].slice(1)
 
         }
-           console.log(firstLetterUpperCase)
     }
-
+    
+    return splitedSentence.join(" ")
 }
 
-titleCase()
 
-// module.exports = {titleCase}
+module.exports = {titleCase}
