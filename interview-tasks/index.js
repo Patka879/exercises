46dfba07f5563a0763e267b10e839ea2567eaad7
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-10-04 16:40:09 +0200

    API array excercise resolved

--- interview-tasks/index.js
+++ interview-tasks/index.js
@@ -11,3 +11,25 @@ function isPangram(str) {
 }
 
 console.log(isPangram("abcdefghijklmnopqrstuvwxyz"))
+
+// API Array Method
+
+const getUsers = async () => {
+    const api = 'https://jsonplaceholder.typicode.com/users'
+    const response = await fetch(api)
+    const json = await response.json()
+    
+    const result = json.filter(v=>v.name.toLowerCase().includes('k')).map(v=>({name:v.name,email:v.email}))
+    
+    return result
+    
+}
+
+(async function() {
+    try{
+     console.log(await getUsers())   
+        
+    }catch(err){
+        console.log(err)
+    }
+})();
