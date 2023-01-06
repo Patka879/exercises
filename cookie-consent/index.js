697d5c4454e53d7d984e10cbf0aee815a3eb2186
Patka879 <mysiak.patrycja@gmail.com>
2023-01-06 18:45:28 +0100

    close btn disabled, buttons reversed

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -3,6 +3,8 @@ const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
 const modalInner = document.getElementById('modal-inner')
+const closeModalBtn = document.getElementById('modal-close-btn')
+const declineBtn = document.getElementById('decline-btn')
 
 setTimeout(function(){
     modal.style.display = 'block'
@@ -31,11 +33,17 @@ consentForm.addEventListener('submit', function(e){
         }, 1500) 
         setTimeout(function(){
             modalInner.innerHTML = `
-            <h2>Thanks ${fullName} you sucker! </h2>
+            <h2>Thanks <span class="modal-display-name"> ${fullName}</span>, you sucker! </h2>
             <p>We just sold the rights to your eternal soul.</p>
             <div class="idiot-gif">
                 <img src="images/hehe-pirate.gif">
             </div>
             `
+            closeModalBtn.disabled = false
+
         }, 3000)
+    })
+
+    declineBtn.addEventListener('mouseover', function(){
+        console.log('btn hovered')
     })
