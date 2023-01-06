d401249575e2dcb4451e848f36e7662d35cfda77
Patka879 <mysiak.patrycja@gmail.com>
2023-01-06 16:25:19 +0100

    functionality added to modal close button

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -1,5 +1,10 @@
 const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
 
 setTimeout(function(){
     modal.style.display = 'block'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
