48531cdcf02d85122e6149438097b49fe4ec3a1e
Patka879 <mysiak.patrycja@gmail.com>
2023-01-06 18:00:50 +0100

    message after submit created

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -1,6 +1,8 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+const modalInner = document.getElementById('modal-inner')
 
 setTimeout(function(){
     modal.style.display = 'block'
@@ -12,5 +14,23 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log('form submitted')
-})
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="upload-text">
+                Uploading your data to the dark web...
+            </p>
+        </div>`
+        setTimeout(function(){
+            document.getElementById('upload-text').innerHTML = `Making the sale...`
+        }, 1500) 
+        setTimeout(function(){
+            modalInner.innerHTML = `
+            <h2>Thanks you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/hehe-pirate.gif">
+            </div>
+            `
+        }, 3000)
+    })
