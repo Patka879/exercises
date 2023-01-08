65585b66569f0d9978151cbb4c3fd3aa8a3b48cd
Patka879 <mysiak.patrycja@gmail.com>
2023-01-08 21:01:13 +0100

    radio buttons added

--- meme-picker/index.js
+++ meme-picker/index.js
@@ -18,10 +18,20 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+                        <div class="radio">
+                            <label for="${emotion}">${emotion}</label>
+                                <input 
+                                    type="radio" 
+                                    id="${emotion}" 
+                                    name="emotions" 
+                                    value="${emotion}"}
+                                >
+                        </div>
+        `
     }
+
     emotionRadios.innerHTML = radioItems
 }
-    
     
 renderEmotionsRadios(catsData)
