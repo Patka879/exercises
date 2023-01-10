3b8d319808ab8a4ffd841db743cad7e3b387e2a0
Patka879 <mysiak.patrycja@gmail.com>
2023-01-10 21:56:20 +0100

    import from data.js corrected, path to img corrected

--- twitter-clone/index.js
+++ twitter-clone/index.js
@@ -1,8 +1,8 @@
-import { tweetsData } from "./data";
+import { tweetsData } from "./data.js"
 
-const tweetInput = document.getElementById('tweet-input')
+
 const tweetBtn = document.getElementById('tweet-btn')
 
 tweetBtn.addEventListener('click', function(){
-    console.log('clicked!')
+    console.log(tweetsData)
 })
