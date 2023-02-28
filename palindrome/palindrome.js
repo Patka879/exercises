906dd9a59f5dad13dd11dd283da0edecada244e2
Patka879 <mysiak.patrycja@gmail.com>
2023-02-28 21:23:08 +0100

    palindrome fixed to accept full sentences

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -10,7 +10,7 @@ function handleChange(e) {
 
 function palindromeCheck (value) {
     value = value.toLowerCase()
-    if (value === value.split("").reverse().join("")) {
+    if (value.replace(/\W/g, "") === value.replace(/\W/g, "").split("").reverse().join("")) {
       return "Yes! It's a palindrome!"
     } else {
       return "No, it's not a palindrome."
